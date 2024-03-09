b27c4215b814ef163f6a7c2d942adaf366bfeb74
Tyron Aricum <tyronaricum1@gmail.com>
2024-03-09 14:54:50 +0200

    update: add docblock to CanLoadRelationship Trait methods

--- app/Http/Traits/CanLoadRelationships.php
+++ app/Http/Traits/CanLoadRelationships.php
@@ -8,13 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 
 trait CanLoadRelationships
 {
+    /**
+     * Load specified relationships into the given query or model.
+     *
+     * @param Model|QueryBuilder|EloquentBuilder $for The query or model to load relationships into.
+     * @param array|null $relations The array of relationships to be loaded.
+     *
+     * @return Model|QueryBuilder|EloquentBuilder The modified query or model with loaded relationships.
+     */
     public function loadRelationships(
         Model|QueryBuilder|EloquentBuilder $for,
         ?array $relations = null
         ): Model|QueryBuilder|EloquentBuilder {
 
             $relations = $relations ?? $this->relations ?? [];
-            
+
             // loop - Load allowed relations into the query or model.
             foreach ($relations as $relation) {
                 $for->when(
@@ -26,6 +34,13 @@ trait CanLoadRelationships
         return $for;
     }
 
+    /**
+     * Determine if the given relation should be included based on the 'include' query parameter.
+     *
+     * @param string $relation The relation to check for inclusion.
+     *
+     * @return bool Whether the relation should be included.
+     */
     protected function shouldIncludeRelation(string $relation): bool
     {
         $include = request()->query('include');
