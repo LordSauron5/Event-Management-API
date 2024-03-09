e95f92c64e2ffff627319982287fd4062d30fb5e
Tyron Aricum <tyronaricum1@gmail.com>
2024-03-09 12:41:52 +0200

    update: create resources for more control

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Models\Event;
 use App\Models\User;
+use App\Models\Event;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 
 class EventController extends Controller
 {
@@ -14,7 +15,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        return EventResource::collection(Event::with('user')->get());
     }
 
     /**
@@ -33,7 +34,7 @@ class EventController extends Controller
             'user_id' => 1
         ]);
 
-        return $event;
+        return new EventResource($event);
     }
 
     /**
@@ -41,7 +42,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return Event::find($event);
+        $event->load('user', 'attendees');
+        return new EventResource($event);
     }
 
     /**
@@ -59,7 +61,7 @@ class EventController extends Controller
             ]),
         );
 
-        return $event;
+        return new EventResource($event);
     }
 
     /**
